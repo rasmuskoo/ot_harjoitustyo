df36f35d95725ec1f10fa65f31bcb96845b040f9
rasmuskoo <rasmus.kaukonen@gmail.com>
2026-03-31 12:48:59 +0300

    Main.py initialization

---
+++ src/main.py
@@ -0,0 +1,10 @@
+"""Application entry point for TaskBoard desktop."""
+
+
+def main() -> None:
+    """Start the application."""
+    print("TaskBoard Desktop version is running.")
+
+
+if __name__ == "__main__":
+    main()
